e68081b1b6daca14576fb7cdfbed808f9ed58231
Sebas Contreras <swf.sebas@gmail.com>
2023-04-03 11:29:11 -0400

    Update LOOP.sql

--- 1 - Bloques Anonimos/3. Loops/LOOP.sql
+++ 1 - Bloques Anonimos/3. Loops/LOOP.sql
@@ -4,7 +4,7 @@ BEGIN
   /*
   Iterador manual: símil al ´DO´ en programación.
   */
-  // llamamos un iterador común y manualmente indicamos cuando debe salirse
+  -- llamamos un iterador común y manualmente indicamos cuando debe salirse
   LOOP 
     dbms_output.put_line(i);
     i:=i+1;
@@ -12,4 +12,13 @@ BEGIN
       EXIT;
     END IF;
   END LOOP;
+  
+  -- Otra forma de usar es dictar la condición de salida.
+  i:=1;
+  LOOP 
+    dbms_output.put_line(i);
+    i:=i+1;
+    EXIT WHEN (i>10);
+  END LOOP;
+  
 END;
